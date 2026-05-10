e75ac9245cb969d1133606123823957ea72af877
Scorpion2018JV <joaovitorvianasegobia@gmail.com>
2026-05-10 17:51:54 -0400

    Passo 6

--- main.js
+++ main.js
@@ -21,16 +21,16 @@ while(controleSair) {
     console.log('4 - Estatísticas')
     console.log('0 - Sair')
 
-    opcao = parseInt(prompt('\nDigite um número: '))
+    opcao = parseInt(prompt('\nDigite número um correspondente a uma opção: '))
 
     if(isNaN(opcao)) {
-        console.log('\nVocê deve digitar apenas números, tente novamente.')
+        console.log('\nVocê deve digitar apenas números, tente novamente.\n')
         continue
     }
 
     switch(opcao) {
         case 1:
-            console.log('\nDigite os dados do livro a ser cadastrado.')
+            console.log('\nDigite os dados do livro a ser cadastrado.\n')
             
             titulo = prompt('Título: ')
             autor = prompt('Autor: ')
@@ -46,7 +46,18 @@ while(controleSair) {
             break;
         
         case 2:
+            console.log()
 
+            if (livros.length === 0) {
+                console.log('Não há livros cadastrados.\n')
+                break
+            }
+
+            livros.forEach((livro, indice) => {
+                console.log(`${indice + 1}. ${livro.getInfo()}`)
+            })
+
+            console.log()
             break;
 
         case 3:
@@ -61,5 +72,8 @@ while(controleSair) {
             console.log("\nObrigado por usar nossos serviços!")
             controleSair = false
             break;
+
+        default:
+            console.log("\nOpção Inválida, tente novamente.\n")
     }
 }
