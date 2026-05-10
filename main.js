5d3683d4c09dd649a3b2f61554e737fbd8c28765
Scorpion2018JV <joaovitorvianasegobia@gmail.com>
2026-05-10 17:11:17 -0400

    Passo 5

--- main.js
+++ main.js
@@ -2,8 +2,16 @@ const prompt = require('prompt-sync')()
 const Livro = require('./Livro')
 
 const livros = []
+
 let opcao;
 let controleSair = true
+
+let titulo;
+let autor;
+let genero;
+let anoPub;
+let numPaginas;
+let novoLivro;
 
 while(controleSair) {
     console.log('--- Biblioteca ---')
@@ -13,16 +21,28 @@ while(controleSair) {
     console.log('4 - Estatísticas')
     console.log('0 - Sair')
 
-    opcao = parseInt(prompt('Digite um número:'))
+    opcao = parseInt(prompt('\nDigite um número: '))
 
     if(isNaN(opcao)) {
-        console.log('Você deve digitar apenas números, tente novamente.')
+        console.log('\nVocê deve digitar apenas números, tente novamente.')
         continue
     }
 
     switch(opcao) {
         case 1:
+            console.log('\nDigite os dados do livro a ser cadastrado.')
+            
+            titulo = prompt('Título: ')
+            autor = prompt('Autor: ')
+            genero = prompt('Gênero: ')
+            anoPub = parseInt(prompt('Ano de publicação: '))
+            numPaginas = parseInt(prompt('Número de páginas: '))
 
+            novoLivro = new Livro(titulo, autor, genero, anoPub, numPaginas)
+
+            livros.push(novoLivro)
+
+            console.log('\nLivro cadastrado com sucesso!\n')
             break;
         
         case 2:
@@ -38,7 +58,7 @@ while(controleSair) {
             break;
         
         case 0:
-            console.log("Obrigado por usar nossos serviços!")
+            console.log("\nObrigado por usar nossos serviços!")
             controleSair = false
             break;
     }
