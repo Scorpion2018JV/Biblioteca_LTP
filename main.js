12ac828fdaf5ea286008aed9e403de4d1e4e99df
Scorpion2018JV <joaovitorvianasegobia@gmail.com>
2026-05-10 16:06:54 -0400

    Passos 2 e 3

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-const prompt = require('prompt-sync')
+const prompt = require('prompt-sync')()
 const Livro = require('./Livro')
 
 const livros = []
+let opcao;
+
+while(true) {
+    console.log('--- Biblioteca ---')
+    console.log('1 - Cadastrar livro')
+    console.log('2 - Listar livros')
+    console.log('3 - Alterar Livro')
+    console.log('4 - Estatísticas')
+    console.log('0 - Sair')
+    opcao = parseInt(prompt('Digite um número:'))
+    if(isNaN(opcao)) {
+        console.log('Você deve digitar apenas números, tente novamente.')
+        continue
+    }
+    break
+}
